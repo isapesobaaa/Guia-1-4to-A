1e5c13afc5353f2a731b0d275756cabe0780ca9e
isapesobaaa <isa.pesoba@gmail.com>
2025-03-25 11:49:17 -0300

    Ejercicios peruga

--- Assets/Scripts/DeclaracionDeVariables.cs
+++ Assets/Scripts/DeclaracionDeVariables.cs
@@ -12,7 +12,17 @@ public class DeclaracionDeVariables : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        canntidadAlumnos = 24;
+        magnitudTerremoto = 9.3f;
+        nombreAlumno = "Jeronimo";
+            compuHabilitada = false;
+
+        Debug.Log(cantidadAlumnos);
+        Debug.log(magnitudTerremoto)       
+        Debug.log(nombreAlumno)        
+        Debug.log(compuHabilitada)
+
+
     }
 
     // Update is called once per frame
